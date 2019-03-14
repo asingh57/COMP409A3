039f1c4daf9da8500b6d6515d9955e8a92c1547c
Abhijit Singh <abhijit.singh@mail.mcgill.ca>
2019-03-14 01:14:41 -0400

    non blocking completed

--- thread_non_blocking.java
+++ thread_non_blocking.java
@@ -1,16 +1,39 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class thread_non_blocking extends processing_thread{
   
+  volatile static AtomicInteger[] priority_holder= new AtomicInteger[priority_count];
+  static{
+    for(int priority=0; priority<priority_count; priority++){
+      priority_holder[priority]=new AtomicInteger(-1);
+    }
+  }
   
   thread_non_blocking(int thread_id){
     this.thread_id=thread_id;
   }
   
   @Override
-  boolean block_or_wait(int priority){    
+  boolean block_or_wait(int priority){
+    
+    while(true){
+      if(priority_holder[priority].compareAndSet(-1,thread_id)){
+        break;
+      }
+      try{
+        Thread.sleep(10);
+      }
+      catch(InterruptedException ex){
+        
+      }
+      
+    }
+    
     return true;
   }
   @Override
-  void release(int priority){    
+  void release(int priority){  
+    priority_holder[priority].set(-1);  
   }
   
 }
