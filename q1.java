13ffcc60ba12a67b864956d307a2d19272e0f2fe
Abhijit Singh <abhijit.singh@mail.mcgill.ca>
2019-03-14 00:58:02 -0400

    bug fix

--- q1.java
+++ q1.java
@@ -3,14 +3,14 @@ public class q1{
   
   public static void main(String[] args){
     int thread_count=Integer.parseInt(args[0]);
-    thread_non_blocking.probability=Float.parseFloat(args[1]);
-    thread_non_blocking.delay=Integer.parseInt(args[2]);
-    thread_non_blocking.number_operations=Integer.parseInt(args[3]);
+    thread_blocking.probability=Float.parseFloat(args[1]);
+    thread_blocking.delay=Integer.parseInt(args[2]);
+    thread_blocking.number_operations=Integer.parseInt(args[3]);
     
     
     Thread thread_array[]= new Thread[thread_count];
     for(int t=0;t<thread_count;t++){
-      thread_array[t]= new Thread(new thread_non_blocking(t));      
+      thread_array[t]= new Thread(new thread_blocking(t));      
     }
     
     for(int t=0;t<thread_count;t++){
--- thread_non_blocking.java
+++ thread_non_blocking.java
@@ -1,7 +1,7 @@
 public class thread_non_blocking extends processing_thread{
   
   
-  thread_non_blocking(int thread_id){   
+  thread_non_blocking(int thread_id){
     this.thread_id=thread_id;
   }
   
